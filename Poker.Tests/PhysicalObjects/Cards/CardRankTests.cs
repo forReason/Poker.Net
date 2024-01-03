a652093b21339ec56a0460fc7b9d3fcafa164cdd
julian b <julian.bechtold@outlook.com>
2024-01-03 13:42:06 +0100

    added tests for card object

---
+++ Poker.Tests/PhysicalObjects/Cards/CardRankTests.cs
@@ -0,0 +1,29 @@
+﻿using Poker.PhysicalObjects.Cards;
+
+public class CardRankTests
+{
+    [Fact]
+    public void CardRanks_AreUnique()
+    {
+        var ranks = Enum.GetValues(typeof(CardRank)).Cast<CardRank>().ToList();
+        var distinctRanks = ranks.Distinct().ToList();
+        Assert.Equal(distinctRanks.Count, ranks.Count);
+    }
+
+    [Theory]
+    [InlineData(CardRank.Two, CardRank.Three)]
+    [InlineData(CardRank.Four, CardRank.Five)]
+    [InlineData(CardRank.Five, CardRank.Six)]
+    [InlineData(CardRank.Six, CardRank.Seven)]
+    [InlineData(CardRank.Seven, CardRank.Eight)]
+    [InlineData(CardRank.Eight, CardRank.Nine)]
+    [InlineData(CardRank.Nine, CardRank.Ten)]
+    [InlineData(CardRank.Ten, CardRank.Jack)]
+    [InlineData(CardRank.Jack, CardRank.Queen)]
+    [InlineData(CardRank.Queen, CardRank.King)]
+    [InlineData(CardRank.King, CardRank.Ace)]
+    public void CardRanks_FollowExpectedOrder(CardRank lower, CardRank higher)
+    {
+        Assert.True((int)lower < (int)higher);
+    }
+}
---
+++ Poker.Tests/PhysicalObjects/Cards/CardSuitTests.cs
@@ -0,0 +1,21 @@
+﻿using Poker.PhysicalObjects.Cards;
+
+namespace Poker.Tests.PhysicalObjects.Cards
+{
+    public class CardSuitTests
+    {
+        [Fact]
+        public void CardSuits_AreUnique()
+        {
+            var suits = Enum.GetValues(typeof(CardSuit)).Cast<CardSuit>().ToList();
+            var distinctSuits = suits.Distinct().ToList();
+            Assert.Equal(distinctSuits.Count, suits.Count);
+        }
+        [Fact]
+        public void CardSuits_AreFour()
+        {
+            var suits = Enum.GetValues(typeof(CardSuit)).Cast<CardSuit>().ToList();
+            Assert.Equal(4, suits.Count);
+        }
+    }
+}
